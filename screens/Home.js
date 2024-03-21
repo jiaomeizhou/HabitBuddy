27fb77ff6ce502fbedcb80be68783288270e2a6e
jiaomeizhou <zhoujiaomei@outlook.com>
2024-03-21 15:51:11 -0700

    add habit category buttons

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button, StyleSheet} from 'react-native'
 import React from 'react'
 
 export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text>Home</Text>
+            <Button title="Create your own habit" onPress={() => alert('Button pressed')} />
+            <View style={styles.buttonsContainer}>
+                <Button title="health" onPress={() => alert('Button pressed')} />
+                <Button title="study" onPress={() => alert('Button pressed')} />
+                <Button title="life" onPress={() => alert('Button pressed')} />
+                <Button title="sport" onPress={() => alert('Button pressed')} />
+                <Button title="work" onPress={() => alert('Button pressed')} />
+            </View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({})
+StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: '50%',
+    },
+});
