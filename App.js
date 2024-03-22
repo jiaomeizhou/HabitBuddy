c3777a74bf5976e5bff4d91c24ff34357b16cf7b
jiaomeizhou <zhoujiaomei@outlook.com>
2024-03-22 14:10:23 -0700

    config firebase auth

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './screens/Home';
 import BottomNavTabs from './components/BottomNavTabs';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useState } from 'react';
+import { auth } from "./firebase-files/firebaseSetup";
 
+import Home from './screens/Home';
+import Signup from './screens/Signup';
+import Login from './screens/Login';
+
+const Stack = createNativeStackNavigator();
 export default function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  console.log("userLoggedIn: ", userLoggedIn);
+
   return (
     <NavigationContainer>
       <BottomNavTabs />
@@ -12,11 +22,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
