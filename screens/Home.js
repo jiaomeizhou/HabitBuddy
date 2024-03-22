1cf2fa423a9b27af3a4d3c81edf012d9cd4680f5
jiaomeizhou <zhoujiaomei@outlook.com>
2024-03-22 11:35:59 -0700

    move the content of home v1 to welcome component

--- screens/Home.js
+++ screens/Home.js
@@ -1,53 +1,11 @@
 import { View, Text, Button, StyleSheet } from 'react-native'
 import React from 'react'
-import { useState } from 'react'
+import Welcome from '../components/Welcome';
 
 export default function Home() {
-    const [selectedCategory, setSelectedCategory] = useState(null);
-
-    const habitsByCategory = {
-        health: ['Exercise', 'Eat Breakfast', 'Drink Water in the morning'],
-        study: ['Read', 'Practice', 'Research'],
-        life: ['Meditate', 'Journal', 'Gratitude'],
-        sport: ['Run', 'Swim', 'Yoga'],
-        work: ['Plan', 'Organize', 'Prioritize'],
-    };
-
-    const renderHabitButtons = () => {
-        let habits = [];
-        if (selectedCategory) {
-            habits = habitsByCategory[selectedCategory];
-        }
-        else {
-            habits = habitsByCategory.health;
-        }
-        return habits.map((habit, index) => (
-            <Button
-                key={index}
-                title={habit}
-                onPress={() => alert(`${habit} button pressed`)}
-            />
-        ));
-    };
 
     return (
-        <View style={styles.container}>
-            <Button
-                title="Create your own habit"
-                onPress={() => alert('Create your own habit button pressed')}
-            />
-            <View style={styles.buttonsContainer}>
-                {Object.keys(habitsByCategory).map((category, index) => (
-                    <Button
-                        key={index}
-                        title={category}
-                        onPress={() => setSelectedCategory(category)}
-                        color={selectedCategory === category ? 'blue' : 'grey'}
-                    />
-                ))}
-            </View>
-            <View style={styles.habitButtonsContainer}>{renderHabitButtons()}</View>
-        </View>
+        <Welcome />
     );
 }
 
@@ -56,18 +14,5 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    buttonsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        width: '50%',
-        marginTop: 10,
-    },
-    habitButtonsContainer: {
-        marginTop: 20,
-        width: '80%',
-    },
-    habitButtonSelected: {
-        backgroundColor: 'blue',
     },
 });
