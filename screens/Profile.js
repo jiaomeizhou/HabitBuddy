78284d007064b716096fc976843def3b0730e14d
jiaomeizhou <zhoujiaomei@outlook.com>
2024-03-22 17:00:36 -0700

    get current user

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { auth } from '../firebase-files/firebaseSetup'
 
 export default function Profile() {
+  console.log("profile page, current user: " , auth.currentUser.email)
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>{auth.currentUser.uid}</Text>
+      <Text>{auth.currentUser.email}</Text>
     </View>
   )
 }
