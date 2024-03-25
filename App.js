bdca0d1211bfba00a706b2fe7f433d3e9a6d6450
unusG <uniclusgao@gmail.com>
2024-03-24 23:10:34 -0700

    Update App.js to add AddHabitScreen and EditHabitScreen

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useState, useEffect } from 'react';
 import { auth } from "./firebase-files/firebaseSetup";
+import AddHabitScreen from './screens/AddHabitScreen';
+import EditHabitScreen from './screens/EditHabitScreen';
 
 
 import Home from './screens/Home';
@@ -37,11 +39,26 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         {userLoggedIn ?
-          <Stack.Screen
-            name="BottomNavTabs"
-            component={BottomNavTabs}
-            options={{ headerShown: false }}
-          /> : AuthStack}
+          <>
+            <Stack.Screen
+              name="BottomNavTabs"
+              component={BottomNavTabs}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="AddHabit" component={AddHabitScreen}
+              options={{
+                headerBackTitleVisible: false,
+                headerTitle: "Add a Habit",
+              }}
+            />
+            <Stack.Screen name="EditHabit" component={EditHabitScreen}
+              options={{
+                headerBackTitleVisible: false,
+                headerTitle: "Edit a Habit",
+              }}
+            />
+          </>
+          : AuthStack}
       </Stack.Navigator>
     </NavigationContainer>
   );
