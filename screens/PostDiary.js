a937c88fc6d211acca319ec19b37fcdf762559df
unusG <uniclusgao@gmail.com>
2024-04-07 18:32:37 -0700

    Update PostDiary to achieve use camera to take a photo and upload img in firebase and fetch img from firebase with updated UI

--- screens/PostDiary.js
+++ screens/PostDiary.js
@@ -1,16 +1,15 @@
-import { StyleSheet, View, Alert, Image } from 'react-native'
+import { StyleSheet, View, Alert, ScrollView } from 'react-native'
 import React, { useState, useCallback } from 'react'
 import { addCheckIn } from '../firebase-files/firestoreHelper';
-import CustomCheckBox from '../components/CustomCheckBox';
 import CustomText from '../components/CustomText';
 import PressableButton from '../components/PressableButton';
-import CustomTextInput from '../components/CustomTextInput';
-import CustomSwitch from '../components/CustomSwitch';
 import { formatDate } from '../helpers/dateHelper';
-import { auth } from '../firebase-files/firebaseSetup';
+import { auth, storage } from '../firebase-files/firebaseSetup';
 import { useFocusEffect } from '@react-navigation/native';
 import ImageManager from '../components/ImageManager';
-
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { TextInput, Switch, Card, HelperText } from 'react-native-paper';
+import * as Colors from '../components/Colors';
 
 export default function PostDiary({ navigation, route }) {
     const [imageUri, setImageUri] = useState(null);
@@ -23,6 +22,15 @@ export default function PostDiary({ navigation, route }) {
     const habitId = habitData.id;
     const date = new Date();
 
+    const inputTheme = {
+        colors: {
+            text: 'seagreen',
+            primary: 'seagreen',
+            underlineColor: 'transparent',
+            background: '#FBFBFB',
+        },
+    };
+
     useFocusEffect(
         useCallback(() => {
             setImageUri(null);
@@ -31,14 +39,29 @@ export default function PostDiary({ navigation, route }) {
             setTaskCompleted(false);
         }, [])
     );
-    function pickImageHandler(uri) {
-        setImageUri(uri);
-        console.log("receive Image URI: ", uri);
+
+    async function getImageData(uri) {
+        try {
+            const response = await fetch(uri);
+            const imageBlob = await response.blob();
+            const imageName = uri.substring(uri.lastIndexOf('/') + 1);
+            const imageRef = ref(storage, `images/${imageName}`);
+            const uploadResult = await uploadBytes(imageRef, imageBlob);
+            return await getDownloadURL(uploadResult.ref);
+        } catch (err) {
+            Alert.alert("Upload Error", "Failed to upload image.");
+        }
+    }
+
+    async function pickImageHandler(uri) {
+        const uploadedImageUrl = await getImageData(uri);
+        if (uploadedImageUrl) {
+            setImageUri(uploadedImageUrl);
+        }
     }
 
     function saveDiary() {
-        // if (!diary.trim() || !imageUri) {
-        if (!diary.trim()) {
+        if (!diary.trim() || !imageUri) {
             Alert.alert("Error", "Please add an image and your diary.");
             return;
         }
@@ -68,61 +91,97 @@ export default function PostDiary({ navigation, route }) {
     }
 
     return (
-        <View style={styles.container}>
-            <CustomText style={styles.date}>{formatDate(date)}</CustomText>
-            <ImageManager receiveImageURI={pickImageHandler} />
-            <CustomTextInput
-                placeholder="Share your diary..."
-                value={diary}
-                onChangeText={setDiary}
-                style={styles.textInput}
-                multiline
-            />
-            <CustomSwitch
-                label="Public : "
-                onValueChange={setIsPublic}
-                value={isPublic}
-            />
-
-            <CustomCheckBox
-                text="Today's Task Completed ? "
-                value={taskCompleted}
-                onValueChange={setTaskCompleted}
-            />
-
-            <View style={styles.buttonsContainer}>
-                <PressableButton title="Save" onPress={saveDiary} color='#2196F3' />
-                <PressableButton title="Cancel" onPress={cancelHandler} color='#B0ADAD' />
-            </View>
-        </View>
-    )
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+            <Card style={styles.card}>
+                <Card.Title title={formatDate(date)} titleNumberOfLines={2} titleStyle={styles.date} />
+                <Card.Content>
+                    <ImageManager receiveImageURI={pickImageHandler} />
+                    <TextInput
+                        label="Share your diary..."
+                        value={diary}
+                        onChangeText={setDiary}
+                        style={styles.textInput}
+                        multiline
+                        mode="outlined"
+                        theme={inputTheme}
+                        outlineStyle={styles.outlineStyle}
+                    />
+                    <View style={styles.row}>
+                        <CustomText style={styles.customText}>Today's Task Completed ? </CustomText>
+                        <Switch
+                            value={taskCompleted}
+                            onValueChange={setTaskCompleted}
+                            color='seagreen'
+                        />
+                    </View>
+                    <View style={styles.row}>
+                        <CustomText style={styles.customText}>Public: </CustomText>
+                        <Switch
+                            value={isPublic}
+                            onValueChange={setIsPublic}
+                            color='seagreen'
+                        />
+                    </View>
+                    <HelperText type="info" style={styles.helperText}>
+                        Making your diary public will allow others to view it.
+                    </HelperText>
+                </Card.Content>
+                <Card.Actions style={styles.cardActions}>
+                    <View style={styles.buttonsContainer}>
+                        <PressableButton title="Save" onPress={saveDiary} color={Colors.fernGreen} textColor={Colors.white} />
+                        <PressableButton title="Cancel" onPress={() => navigation.goBack()} color={Colors.white} textColor={Colors.fernGreen} />
+                    </View>
+                </Card.Actions>
+            </Card>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    customText: {
+        color: '#5A7247',
+    },
+    helperText: {
+        fontSize: 13,
+        color: '#5A7247',
+    },
+    scrollViewContent: {
+        flexGrow: 1,
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20,
+        padding: 12,
+        marginTop: 10,
+    },
+    card: {
+        margin: 8,
+        elevation: 4,
+        backgroundColor: 'rgba(232 241 226 / 0.9)'
+    },
+    cardActions: {
+        justifyContent: 'space-around',
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 8,
     },
     date: {
         fontSize: 18,
-        marginBottom: 20,
+        margin: 10,
         fontWeight: 'bold',
-    },
-    image: {
-        width: 200,
-        height: 200,
-        resizeMode: 'contain',
-        marginVertical: 20,
+        textAlign: 'center',
+        color: '#5A7247',
     },
     textInput: {
-        height: 100,
-        width: '100%',
+        height: 80,
         marginVertical: 10,
-        borderWidth: 1,
-        borderColor: '#A9A4A4',
         padding: 10,
+    },
+    outlineStyle: {
+        borderWidth: 1,
+        borderColor: '#5A7247',
+        borderRadius: 10,
     },
     buttonsContainer: {
         marginVertical: 30,
